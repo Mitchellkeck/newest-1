fe87113dc54fbec3aff6709a7199ec30d3d66691
HECVZCU <HECVZCU@CCSD-459YBH2.ccsdut.net>
2019-01-14 09:59:43 -0700

    commit

---
+++ Test/src/mainClas.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author HECVZCU
+ *
+ */
+public class mainClas
+{
+	public static void main(String[] args)
+	{
+		System.out.println("okay");
+		
+		
+		
+	}
+
+}
---
+++ Test/src/mainClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author HECVZCU
+ *
+ */
+public class mainClass
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
